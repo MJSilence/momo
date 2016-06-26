08def40f35d9706f04f032737e4683b14f80847d
lmj <lmj@qq.com>
2016-06-26 17:39:43 +0800

    创建了cat类

---
+++ momo/momo/MJCat.h
@@ -0,0 +1,13 @@
+//
+//  MJCat.h
+//  momo
+//
+//  Created by 林孟杰 on 16/6/26.
+//  Copyright © 2016年 LMJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MJCat : NSObject
+
+@end
---
+++ momo/momo/MJCat.m
@@ -0,0 +1,13 @@
+//
+//  MJCat.m
+//  momo
+//
+//  Created by 林孟杰 on 16/6/26.
+//  Copyright © 2016年 LMJ. All rights reserved.
+//
+
+#import "MJCat.h"
+
+@implementation MJCat
+
+@end
