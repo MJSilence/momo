bd1975b86f365cb7bc6b5b44ac1d151fbcbc5ffc
lmj <lmj@qq.com>
2016-06-26 17:35:19 +0800

    初始化项目

---
+++ momo/momo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  momo
+//
+//  Created by 林孟杰 on 16/6/26.
+//  Copyright © 2016年 LMJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
