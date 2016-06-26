1dd13c957e54764207672af31c419ca7a7da410e
lmj <lmj@qq.com>
2016-06-26 17:38:13 +0800

    开始开发

--- momo/momo/ViewController.m
+++ momo/momo/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"开始开发");
 }
 
 - (void)didReceiveMemoryWarning {
